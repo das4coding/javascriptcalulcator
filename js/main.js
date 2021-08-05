0a8d30919d7b7f8d8f9e753f4a6ef83b3f1e71c8
DAS <bd_chauhan@live.com>
2021-08-05 17:38:36 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var ttxbox = document.getElementById("txtbox");
+
+//button 0
+document.getElementById("btn0").addEventListener("click",function(){
+    document.getElementById("txtbox").value += "0";
+
+});
+
+
+//button 00
+document.getElementById("btn00").addEventListener("click",function(){
+    document.getElementById("txtbox").value += "00";
+
+});
+
+
+//button 000
+document.getElementById("btn000").addEventListener("click",function(){
+    document.getElementById("txtbox").value += "000";
+
+});
+
+
+
+//button 1
+document.getElementById("btn1").addEventListener("click",function(){
+    document.getElementById("txtbox").value += 1;
+
+});
+
+//button 2
+document.getElementById("btn2").addEventListener("click",function(){
+    document.getElementById("txtbox").value += 2;
+
+});
+
+//button 3
+document.getElementById("btn3").addEventListener("click",function(){
+    document.getElementById("txtbox").value += 3;
+
+});
+
+//button 4
+document.getElementById("btn4").addEventListener("click",function(){
+    document.getElementById("txtbox").value += 4;
+
+});
+
+//button 5
+document.getElementById("btn5").addEventListener("click",function(){
+    document.getElementById("txtbox").value += 5;
+
+});
